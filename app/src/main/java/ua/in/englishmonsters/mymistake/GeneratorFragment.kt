3125ea1f474270760e41a59cbdc2552cef68347a
pchupik <pchupik@gmail.com>
2020-05-14 17:05:15 +0300

    - minor changes to Generator UI

--- app/src/main/java/ua/in/englishmonsters/mymistake/GeneratorFragment.kt
+++ app/src/main/java/ua/in/englishmonsters/mymistake/GeneratorFragment.kt
@@ -4,14 +4,14 @@ import android.Manifest
 import android.app.Activity
 import android.content.Intent
 import android.content.pm.PackageManager
-import android.graphics.*
+import android.graphics.Bitmap
+import android.graphics.Matrix
 import android.net.Uri
 import android.os.Bundle
 import android.provider.MediaStore
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
@@ -40,6 +40,7 @@ class GeneratorFragment : Fragment() {
         viewModel.getCardData().observe(viewLifecycleOwner, Observer {
             nameEditText.setText(it.name)
             photoImageView.setClipToOutline(true)
+            if (it.photo != null)
             photoImageView.setImageBitmap(it.photo)
         })
 
